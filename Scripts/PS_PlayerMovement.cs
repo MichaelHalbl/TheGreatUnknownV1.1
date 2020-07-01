556b157976928456c5e01b480c10618c3763a1d1
paulineSt <67285385+paulineSt@users.noreply.github.com>
2020-07-01 12:36:10 +0200

    Add files via upload

---
+++ Scripts/PS_PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS_PlayerMovement : MonoBehaviour
+{
+
+
+    public CharacterController control;
+
+    public float speed = 12f;
+
+    public float gravity = -9.81f *2;
+    
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
+    public float jumphight = 2f;
+
+    bool crouched = false;
+    Vector3 velocity;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded= Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+
+            velocity.y = -1f;
+        }
+
+        
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x +transform.forward *z;
+
+        control.Move(move * speed* Time.deltaTime);  
+
+
+        if(Input.GetButtonDown("Jump") && isGrounded){  //jump
+
+            velocity.y = Mathf.Sqrt(jumphight * -2 * gravity);
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.C)){
+        
+         if(crouched){    //try to stand up
+
+            Vector3 top = new Vector3(transform.position.x, transform.position.y+control.height, transform.position.z); 
+
+            var canStand = Physics.Raycast(top, Vector3.up, 1f);
+            Debug.Log(transform.position);
+            Debug.Log(canStand);
+
+            
+
+            if(!canStand){
+            Debug.Log("Not Crouching");
+            control.height = 2f;
+            control.center = new Vector3 (control.center.x, 0f, control.center.z);
+            crouched = false;
+            }else{
+
+                Debug.Log("cant Stand Up");
+
+            }
+
+
+         }else{         //crouch
+
+            Debug.Log(transform.position);
+
+            Debug.Log("Crouching");
+            control.height = 1f;
+            control.center = new Vector3 (control.center.x, 0.5f, control.center.z);
+            crouched = true;
+
+         }
+
+        }
+
+    
+        velocity.y += gravity*Time.deltaTime;
+
+        control.Move(velocity * Time.deltaTime);
+    
+    }
+
+
+}
---
+++ Scripts/PS_movingPlattform.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS_movingPlattform : MonoBehaviour
+{
+
+    public Vector3[] points;
+    public int point_number = 0;
+    private Vector3 current_target;
+    public float tolerance; // snap clean to final destination
+    public float speed; // Geschwindigkeit der Plattform
+    public float delay_time; // wann bewegt sich die Plattform wieder zurück
+
+    public float delay_start;
+    public bool automatic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(points.Length > 0){
+            current_target = points[0];
+        }
+        tolerance = speed * Time.deltaTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != current_target){
+            MovePlattform();
+        }else{
+            Updatelarget();
+        }
+    }
+
+    void MovePlattform(){
+        Vector3 heading = current_target - transform.position;
+        transform.position += (heading/heading.magnitude) * speed * Time.deltaTime;
+        if(heading.magnitude < tolerance){
+            transform.position = current_target;
+            delay_start = Time.time;
+        }
+    }
+
+    void Updatelarget(){
+        if(automatic){
+            if(Time.time - delay_start > delay_time){
+                NextPlatform();
+            }
+        }
+    }
+
+    public void NextPlatform(){
+        point_number++;
+        if(point_number >= points.Length){
+            point_number =0;
+        }
+        current_target = points[point_number];
+    }
+
+    private void OnTriggerEnter(Collider other){
+        other.transform.parent= transform;
+    }
+    private void OnTriggerExit(Collider other){
+        other.transform.parent = null;
+    }
+}
