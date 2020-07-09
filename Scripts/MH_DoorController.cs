be873bd75f8bf1424058fe611b49453246f7ba1d
Michael H <63379713+MichaelHalbl@users.noreply.github.com>
2020-07-09 16:33:33 +0200

    DoorController
    DoorController

---
+++ Scripts/MH_DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MH_DoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool doorIsOpening;
+    
+    void Update()
+    {
+        // Wenn der boolean wert true liefert öffnet sich die Tür
+        if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        // Wenn der wert größer 7f ist wird die tür gestoppt
+        if (Door.transform.position.y > 4.1f)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+}
